8066f2721441ef9eb645e60ec64e216a52a9ecb9
tighiltfd <tighiltfd@gmail.com>
2025-02-25 19:48:02 -0500

    Create SonarQube.groovy

---
+++ SonarQube.groovy
@@ -0,0 +1,30 @@
+pipeline:
+pipeline{
+    agent any
+    environment {
+        PATH = "$PATH:/opt/maven/bin"
+    }
+    stages{
+       stage('GetCode'){
+            steps{
+                git 'https://github.com/monzureelahi/helloworld.git'
+            }
+         }        
+       stage('Build'){
+            steps{
+                sh 'mvn clean package'
+            }
+         }
+        stage('SonarQube analysis') {
+//    def scannerHome = tool 'SonarScanner 4.0';
+        steps{
+        withSonarQubeEnv('sonarqube-8.9') { 
+        // If you have configured more than one global server connection, you can specify its name
+//      sh "${scannerHome}/bin/sonar-scanner"
+        sh "mvn sonar:sonar"
+    }
+        }
+        }
+       
+    }
+}
